386b8df3a76ff0f3f14de14fe6df860af7d8d828
Nicole <nicole.loescher91@gmail.com>
2023-06-19 17:04:48 -0700

    memlock title

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import LandingPage from "./components/landingPage";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<LandingPage />} />
+    // </Routes>
+    <div>hello memlock</div>
   );
 }
 
