758cb77bf5e1e52bd6bff1e85263d7209f18ddad
Nicole <nicole.loescher91@gmail.com>
2023-06-19 17:18:33 -0700

    added back

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import LandingPage from "./components/landingPage";
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<LandingPage />} />
-    // </Routes>
-    <div>hello memlock</div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+    // <div>hello memlock</div>
   );
 }
 
