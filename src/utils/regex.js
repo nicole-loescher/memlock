d5bdd9894d4acbcbbc8717b9738a98d28c297bef
Nicole <nicole.loescher91@gmail.com>
2023-08-08 09:57:58 -0700

    app

--- src/utils/regex.js
+++
@@ -1,7 +0,0 @@
-export const PASSWORD_REGEX = new RegExp("^(?=.*)(?=.*[a-z])(?=.*[A-Z]).{8,}$");
-
-export const EMAIL_REGEX = new RegExp(
-  '/^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/'
-);
-
-export const NAME_REGEX = new RegExp("/^[a-zA-Zs.]*$/");
